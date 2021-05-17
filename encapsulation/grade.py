5b15dcc950ede901c5e9073b82eef8f9e16a303b
marattang <tlawlfp0322@gmail.com>
2021-05-17 17:11:10 +0900

    first commit

---
+++ encapsulation/grade.py
@@ -0,0 +1,18 @@
+class Grade:
+
+    def setGrade(self, mat, eng, kor):
+        self.mat = mat
+        self.eng = eng
+        self.kor = kor
+
+    def sum(self):
+        return self.mat+self.eng+self.kor
+
+    def avg(self):
+        return self.sum()/3
+
+if __name__ == '__main__':
+    g = Grade()
+    g.setGrade(90, 85, 78)
+    print(g.avg())
+    print(g.sum())
